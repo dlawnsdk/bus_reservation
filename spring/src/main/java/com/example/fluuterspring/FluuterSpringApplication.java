2e07294565b56c43077919655f6772cfef987780
Juna Im <dlawnsdk04@naver.com>
2023-08-08 23:35:31 +0900

    study spring boot

--- spring/src/main/java/com/example/fluuterspring/FluuterSpringApplication.java
+++ spring/src/main/java/com/example/fluuterspring/FluuterSpringApplication.java
@@ -3,7 +3,11 @@ package com.example.fluuterspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,9 +17,22 @@ public class FluuterSpringApplication {
         SpringApplication.run(FluuterSpringApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+        return "Hello " + name;
     }
 
+    @GetMapping("/player")
+    public Player getPlayer(){
+        return new Player("Im jun a", 42);
+    }
+
+    @GetMapping("/players")
+    public List<Player> getPlayers(){
+        final List<Player> players = new ArrayList<>();
+        players.add(new Player("Im jun a", 42));
+        players.add(new Player("Yoon su bin", 24));
+
+        return players;
+    }
 }
