bc5910912fc1b60f68c1dfb40f60051a16ddc794
Juna Im <dlawnsdk04@naver.com>
2023-08-28 20:41:45 +0900

    study spring boot

---
+++ spring/src/main/java/com/example/fluuterspring/security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.fluuterspring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET)
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
